0e98163daf481ac64686ebcd7042d1f41f856806
Cbustamante777 <cra.bustamante@alumnos.duoc.cl>
2020-02-15 21:22:29 -0300

    guardando la tareas en json

--- por-hacer/por-hacer.js
+++ por-hacer/por-hacer.js
@@ -2,6 +2,18 @@ const fs = require('fs');
 
 let listadoPorHacer = [];
 
+const guardarDb = () =>{
+    //pasar el arreglo a un json
+    let data = JSON.stringify(listadoPorHacer);
+
+    fs.writeFile('db/data.json', data, (err)=>{
+        //esto resume hacer el if
+        if (err) throw new Error('no se pudo grabar', err);
+            
+        
+    })
+}
+
 //funcion que sirve para crear una nota
 const crear = (descripcion) => {
     let porHacer = {
@@ -11,7 +23,9 @@ const crear = (descripcion) => {
     };
 
     //agregando el objeto al arreglo de listado
-    listadoPorHacer.push(porHacer)
+    listadoPorHacer.push(porHacer);
+    //luego de guarda en el arreglo pasa a estar guarda en db
+    guardarDb();
 
     return porHacer;
 }
