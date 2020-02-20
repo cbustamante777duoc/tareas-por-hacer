a28334a02e431ec3eb7cc2df26d45759ca13c0a0
Cbustamante777 <cra.bustamante@alumnos.duoc.cl>
2020-02-19 22:57:40 -0300

    optimizaciones de codigo

---
+++ README.md
@@ -0,0 +1,12 @@
+## aplicacion de node
+
+app contiene un crud,
+uso de yargs,
+base de datos local (json)
+
+para ejecutar usar el siguiente comando
+para descargar los modulos de node_module
+
+```
+npm install
+```
--- config/yargs.js
+++ config/yargs.js
@@ -1,35 +1,26 @@
-const opciones = {
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'descripcion de la tarea por hacer'
+}
+
+const completado = {
+    demand: true,
+    alias: 'd',
+    desc: 'descripcion de la tarea por hacer'
 
 }
 
-
 const argv = require('yargs')
                     .command('crear', 'crear notas',{
-                       descripcion:{
-                            demand:true,
-                            alias: 'd',
-                            desc:'descripcion de la tarea por hacer'
-                       }
+                       descripcion
                     })
                     // este necesita dos comando por eso son dos objectos
                     .command('actualizar','actualizar nota', {
-                        descripcion:{
-                            demand:true,
-                            alias: 'd',
-                            desc:'descripcion de la tarea por hacer'
-                       },
-                       completado:{
-                           default:true,
-                           alias:'c',
-                           desc:'marca como completada las notas '
-                       }
+                        descripcion,completado
                     })
                     .command('borrar', 'borrar nota',{
-                        descripcion:{
-                            demand:true,
-                            alias: 'd',
-                            desc:'descripcion de la tarea por hacer'
-                       }
+                        descripcion
                     })
                     .help()
                     .argv;
